34728c716ae87ed5fbf33669402b989383419231
520MianXiangDuiXiang520 <15364968962@163.com>
2020-01-07 13:19:50 +0800

    :sparkles:完成超级用户登录前端页面

---
+++ web/static/depend/js/dama.js
@@ -0,0 +1,56 @@
+/****************************************************************
+ *																*
+ * 						      浠ｇ爜搴�							*
+ *                        www.dmaku.com							*
+ *       		  鍔姏鍒涘缓瀹屽杽銆佹寔缁洿鏂版彃浠朵互鍙婃ā鏉�			*
+ * 																*
+ ****************************************************************/
+var current = null;
+document.querySelector('#email').addEventListener('focus', function(e) {
+    if (current) current.pause();
+    current = anime({
+        targets: 'path',
+        strokeDashoffset: {
+            value: 0,
+            duration: 700,
+            easing: 'easeOutQuart'
+        },
+        strokeDasharray: {
+            value: '240 1386',
+            duration: 700,
+            easing: 'easeOutQuart'
+        }
+    });
+});
+document.querySelector('#password').addEventListener('focus', function(e) {
+    if (current) current.pause();
+    current = anime({
+        targets: 'path',
+        strokeDashoffset: {
+            value: -336,
+            duration: 700,
+            easing: 'easeOutQuart'
+        },
+        strokeDasharray: {
+            value: '240 1386',
+            duration: 700,
+            easing: 'easeOutQuart'
+        }
+    });
+});
+document.querySelector('#submit').addEventListener('focus', function(e) {
+    if (current) current.pause();
+    current = anime({
+        targets: 'path',
+        strokeDashoffset: {
+            value: -730,
+            duration: 700,
+            easing: 'easeOutQuart'
+        },
+        strokeDasharray: {
+            value: '530 1386',
+            duration: 700,
+            easing: 'easeOutQuart'
+        }
+    });
+});
